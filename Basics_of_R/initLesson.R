48e4c55ab604bbb4c9e792347866d0bb00897735
Kural <kuralarasan.kumar@ugent.be>
2025-06-24 15:06:12 +0200

    Initial commit

---
+++ Basics_of_R/initLesson.R
@@ -0,0 +1,10 @@
+# Load the synthetic criminology dataset package
+library(dplyr)
+library(crimsyndata)
+
+# Load datasets from the package into the lesson environment
+data("crime_stats_ghent")
+data("neighborhood_index")
+data("fear_of_crime_survey")
+data("police_effort_index")
+data("crime_journal_notes")
---
+++ Basics_of_R/swirl.R
@@ -0,0 +1,21 @@
+install.packages("remotes")  # If not already installed
+remotes::install_github("KKural/crimsyndata")
+install.packages("swirlify")
+install.packages("swirl")
+
+library(crimsyndata)
+library(swirlify)
+set_lesson("C:/Users/kukumar/OneDrive - UGent/Job/Data_Analysis_Bachelor_Criminology/Basics_of_R/lesson.yaml")
+test_lesson()
+
+demo_lesson()
+bye()
+
+library(swirl)
+swirl()
+
+
+install.packages("remotes") # if not already installed
+install_course_github("KKural", "crime-analysis-swirl")
+library(swirl)
+swirl()
