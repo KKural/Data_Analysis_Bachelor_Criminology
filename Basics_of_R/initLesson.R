76d1d16a9340ff828ee68c9d68354cce03bf3916
Kural <kuralarasan.kumar@ugent.be>
2025-06-25 01:58:37 +0200

    Refactor dataset loading and update lesson content for T-Test and Chi-Square lesson

--- Basics_of_R/initLesson.R
+++ Basics_of_R/initLesson.R
@@ -1,13 +1,13 @@
 # Load the synthetic criminology dataset package
-library(dplyr)
-if (!require(crimsyndata)) remotes::install_github("KKural/crimsyndata")
+library(dplyr) # Only load if needed for lesson code
 library(crimsyndata)
 
 # Load datasets from the package into the lesson environment
-data("crime_stats_ghent")
-data("neighborhood_index")
-data("fear_of_crime_survey")
-data("police_effort_index")
-data("crime_journal_notes")
+data("crime_stats_ghent", package = "crimsyndata")
+data("neighborhood_index", package = "crimsyndata")
+data("fear_of_crime_survey", package = "crimsyndata")
+data("police_effort_index", package = "crimsyndata")
+data("crime_journal_notes", package = "crimsyndata")
 
+# If you want a local copy for manipulation:
 df_crime_stats_ghent <- crime_stats_ghent
--- T_Test_and_Chi_Square/initLesson.R
+++ T_Test_and_Chi_Square/initLesson.R
@@ -1,10 +1,10 @@
 # Load the synthetic criminology dataset package
-library(dplyr)
+library(dplyr) # Only load if needed for lesson code
 library(crimsyndata)
 
 # Load datasets from the package into the lesson environment
-data("crime_stats_ghent")
-data("neighborhood_index")
-data("fear_of_crime_survey")
-data("police_effort_index")
-data("crime_journal_notes")
+data("crime_stats_ghent", package = "crimsyndata")
+data("neighborhood_index", package = "crimsyndata")
+data("fear_of_crime_survey", package = "crimsyndata")
+data("police_effort_index", package = "crimsyndata")
+data("crime_journal_notes", package = "crimsyndata")
