5653f1c8b5ef4042bf0b88ec213088c94e17a14e
Kural <kuralarasan.kumar@ugent.be>
2025-06-25 02:46:25 +0200

    Remove loading of unused datasets in initLesson.R

--- T_Test_and_Chi_Square/initLesson.R
+++ T_Test_and_Chi_Square/initLesson.R
@@ -5,10 +5,6 @@ library(crimsyndata)
 # Load datasets from the package into the lesson environment
 data("crime_stats_ghent", package = "crimsyndata")
 data("neighborhood_index", package = "crimsyndata")
-data("fear_of_crime_survey", package = "crimsyndata")
-data("police_effort_index", package = "crimsyndata")
 
 crime_stats_ghent <- crime_stats_ghent
 neighborhood_index <- neighborhood_index
-fear_of_crime_survey <- fear_of_crime_survey
-police_effort_index <- police_effort_index
