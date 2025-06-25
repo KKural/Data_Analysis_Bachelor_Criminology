5cdf14dec42bdd58f68bc45502721ff6cd7a5b45
Kural <kuralarasan.kumar@ugent.be>
2025-06-25 03:02:51 +0200

    Remove custom tests and unnecessary dataset loading in initLesson.R

--- T_Test_and_Chi_Square/initLesson.R
+++ T_Test_and_Chi_Square/initLesson.R
@@ -1,7 +1,2 @@
 # Load the synthetic criminology dataset package
 library(crimsyndata)
-
-# Load datasets from the package into the lesson environment
-data("crime_stats_ghent", package = "crimsyndata")
-
-crime_stats_ghent <- crime_stats_ghent
