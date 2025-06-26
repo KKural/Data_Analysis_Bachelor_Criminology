4134243f6179d1b45888f506ed492e80b962d9bf
KKural <122997366+KKural@users.noreply.github.com>
2025-06-26 23:15:56 +0200

    Update initLesson.R

--- Basics_of_R/initLesson.R
+++ Basics_of_R/initLesson.R
@@ -1,12 +1,15 @@
 # Load the synthetic criminology dataset package
+library(dplyr) # Only load if needed for lesson code
 library(crimsyndata)
+library(flextable)
 
 # Load datasets from the package into the lesson environment
 data("crime_stats_ghent", package = "crimsyndata")
 data("neighborhood_index", package = "crimsyndata")
 data("fear_of_crime_survey", package = "crimsyndata")
 data("police_effort_index", package = "crimsyndata")
-data("crime_journal_notes", package = "crimsyndata")
 
-# If you want a local copy for manipulation:
-df_crime_stats_ghent <- crime_stats_ghent
+crime_stats_ghent <- crime_stats_ghent
+neighborhood_index <- neighborhood_index
+fear_of_crime_survey <- fear_of_crime_survey
+police_effort_index <- police_effort_index
